300764097f1be3f2d9c65a937f54978b743ca2d8
frederic <bemeraude@hotmail.com>
2023-08-07 11:17:32 -0400

    m

--- data/SST2/SST2Dataset.py
+++ data/SST2/SST2Dataset.py
@@ -35,8 +35,8 @@ class SST2_dataset(Dataset):
 		# mask=len(argdict['categories'])
 		for i, row in data.iterrows():
 			input = tokenizer.tokenize("<bos> " + row['sentence'].lower() + " <eos>")
-			# if len(input)>self.max_len:
-			# 	self.max_len=len(input)
+			if len(input)>self.max_len:
+				self.max_len=len(input)
 			self.data[index] = {'sentence':row['sentence'].lower(), 'input': input, 'label':row['label']}
 			index+=1
 
