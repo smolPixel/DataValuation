98e8a5ca9c95a0ad6ed62cea2882c56832bd1223
frederic <bemeraude@hotmail.com>
2023-08-07 11:06:11 -0400

    m

--- LOO.py
+++ LOO.py
@@ -27,57 +27,67 @@ def main():
     train, dev, test=initialize_dataset(DATASET_SIZE)
     print(f"Initialized SST-2 with length of {len(train)}")
 
-    print("Running LOO with LogReg classifier")
+    # print("Running LOO with LogReg classifier")
+    #
+    # from Classifier.LogReg import LogReg_Classifier
+    # set_seed()
+    # Classifier = LogReg_Classifier(train)
+    # results=Classifier.train_test(train, dev, test)
+    # dev_baseline=results[1]
+    #
+    # print(f"Results with all data is {results}")
+    #
+    # results=[]
+    # for i in range(DATASET_SIZE):
+    #     train_loo=copy.deepcopy(train)
+    #     train_loo.data.pop(i)
+    #     set_seed()
+    #     Classifier = LogReg_Classifier(train_loo)
+    #     _, dev_res, _ = Classifier.train_test(train_loo, dev, test)
+    #     #If the perfo augments when removing (if diff is positive), then this was a bad data
+    #     results.append(dev_res-dev_baseline)
+    #
+    # sorted_results=np.argsort(results)
+    # # print(sorted_results)
+    # # print(sorted_results[::-1])
+    # print("Evaluation of LOO, removing best data by bs of 10")
+    # for i in range(0, 50, 5):
+    #     train_eval=copy.deepcopy(train)
+    #     eliminated=0
+    #     for ss in sorted_results:
+    #         if eliminated == i:
+    #             print(f"Testing with dataset of size of {len(train_eval)}")
+    #             break
+    #         train_eval.data.pop(ss)
+    #         eliminated+=1
+    #     set_seed()
+    #     Classifier = LogReg_Classifier(train_eval)
+    #     _, dev_res, _ = Classifier.train_test(train_eval, dev, test)
+    #     print(f"Results of {dev_res}")
+    # print("Evaluation of LOO, removing worst data by bs of 10")
+    # for i in range(0, 50, 5):
+    #     train_eval = copy.deepcopy(train)
+    #     eliminated = 0
+    #     for ss in sorted_results[::-1]:
+    #         if eliminated == i:
+    #             print(f"Testing with dataset of size of {len(train_eval)}")
+    #             break
+    #         train_eval.data.pop(ss)
+    #         eliminated += 1
+    #     set_seed()
+    #     Classifier = LogReg_Classifier(train_eval)
+    #     _, dev_res, _ = Classifier.train_test(train_eval, dev, test)
+    #     print(f"Results of {dev_res}")
 
-    from Classifier.LogReg import LogReg_Classifier
+    print("Running LOO with RNN classifier")
+
+    from Classifier.RNN import RNN_Classifier
     set_seed()
-    Classifier = LogReg_Classifier(train)
-    results=Classifier.train_test(train, dev, test)
-    dev_baseline=results[1]
+    Classifier = RNN_Classifier(train)
+    results = Classifier.train_test(train, dev, test)
+    dev_baseline = results[1]
 
     print(f"Results with all data is {results}")
-
-    results=[]
-    for i in range(DATASET_SIZE):
-        train_loo=copy.deepcopy(train)
-        train_loo.data.pop(i)
-        set_seed()
-        Classifier = LogReg_Classifier(train_loo)
-        _, dev_res, _ = Classifier.train_test(train_loo, dev, test)
-        #If the perfo augments when removing (if diff is positive), then this was a bad data
-        results.append(dev_res-dev_baseline)
-
-    sorted_results=np.argsort(results)
-    # print(sorted_results)
-    # print(sorted_results[::-1])
-    print("Evaluation of LOO, removing best data by bs of 10")
-    for i in range(0, 50, 5):
-        train_eval=copy.deepcopy(train)
-        eliminated=0
-        for ss in sorted_results:
-            if eliminated == i:
-                print(f"Testing with dataset of size of {len(train_eval)}")
-                break
-            train_eval.data.pop(ss)
-            eliminated+=1
-        set_seed()
-        Classifier = LogReg_Classifier(train_eval)
-        _, dev_res, _ = Classifier.train_test(train_eval, dev, test)
-        print(f"Results of {dev_res}")
-    print("Evaluation of LOO, removing worst data by bs of 10")
-    for i in range(0, 50, 5):
-        train_eval = copy.deepcopy(train)
-        eliminated = 0
-        for ss in sorted_results[::-1]:
-            if eliminated == i:
-                print(f"Testing with dataset of size of {len(train_eval)}")
-                break
-            train_eval.data.pop(ss)
-            eliminated += 1
-        set_seed()
-        Classifier = LogReg_Classifier(train_eval)
-        _, dev_res, _ = Classifier.train_test(train_eval, dev, test)
-        print(f"Results of {dev_res}")
 
     # results_train_iter, results_dev_iter, results_test_iter =classifier_algo.train_test(train, dev, test)
 
