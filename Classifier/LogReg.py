425d8cd09c9c2135e30ec9107fbb0d95459f0b08
frederic <bemeraude@hotmail.com>
2023-08-07 09:35:06 -0400

    m

--- Classifier/LogReg.py
+++ Classifier/LogReg.py
@@ -1,6 +1,7 @@
 import os
 from sklearn.metrics import accuracy_score, recall_score, f1_score
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 import itertools
 
 class LogReg_Classifier():
@@ -14,10 +15,12 @@ class LogReg_Classifier():
         sents=list(self.train.return_pandas()['sentence'])
         self.vectorizer=TfidfVectorizer()
         self.vectorizer.fit(sents)
+        self.clf=LogisticRegression()
 
     def train_test(self, train, dev, test):
         train=train.return_pandas()
         X=self.vectorizer.transform(list(train['sentence']))
         Y=list(train['label'])
-        print(X, Y)
-        
+        self.clf.fit(X, Y)
+        preds=self.clf.predict(X)
+        print(preds)
