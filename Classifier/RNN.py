32c891b2bd6b167c237cd090a56361815a09d1c5
frederic <bemeraude@hotmail.com>
2023-08-08 09:18:38 -0400

    m

--- Classifier/RNN.py
+++ Classifier/RNN.py
@@ -64,7 +64,7 @@ class RNN_Classifier():
         self.optimizer = AdamW(self.model.parameters(), lr=1e-5)
 
     def train_test(self, train, dev, test):
-        for ep in range(20):
+        for ep in range(50):
             train_loader = DataLoader(
                 dataset=train,
                 batch_size=25,
