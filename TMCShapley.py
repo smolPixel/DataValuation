4d9277d5c751d77e5322533ade2ddbfe819c0c00
frederic <bemeraude@hotmail.com>
2023-08-28 10:25:50 -0400

    m

--- TMCShapley.py
+++ TMCShapley.py
@@ -36,9 +36,9 @@ def TMC_Shapley(train, dev, test, classifier_algo, dev_baseline):
     #dev_baseline= V(D) in paper
 
     #For now let's put PT at 2%, aka, when we get at 2% of the value of dev_baseline we are satisfied
-    PT=0.02
+    PT=0.01
     TRUNC_MAX=5
-    ITER_NUM=10
+    ITER_NUM=50
 
     # train_iter=copy.deepcopy(train)
     # permuatation=train_iter.permute_data()
