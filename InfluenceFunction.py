f6cde90c8f70195a7f9116405091178832e68388
frederic <bemeraude@hotmail.com>
2023-08-29 09:27:51 -0400

    auto commit, minor modification

--- InfluenceFunction.py
+++ InfluenceFunction.py
@@ -1,5 +1,4 @@
-"""Fast Distributional Shapley as in https://arxiv.org/pdf/2002.12334.pdf. AFAK, same as DataOOB but with a weighting scheme to privilege
-sampling small subsets"""
+"""Calculating values with Influence Function, following https://arxiv.org/pdf/2004.11546.pdf"""
 
 
 #Main file
@@ -9,6 +8,7 @@ import argparse
 import random
 import numpy as np
 import torch
+import math
 import matplotlib.pyplot as plt
 import yaml
 import time
@@ -19,10 +19,12 @@ from tqdm import tqdm
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import auc
 
 from Classifier.LogReg import LogReg_Classifier
 from Classifier.RNN import RNN_Classifier
 from Classifier.BERT import Bert_Classifier
+from torch.utils.data import DataLoader
 def set_seed(seed=42):
     random.seed(seed)
     np.random.seed(seed)
@@ -30,64 +32,53 @@ def set_seed(seed=42):
     torch.cuda.manual_seed_all(seed)
 
 
-def sample_num_data(num_data):
-    weights = [num_data - i for i in range(num_data)]
-    weights = [ww / sum(weights) for ww in weights]
-    cum_sum = [sum(weights[0:x:1]) for x in range(1, len(weights) + 1)]
-    # print(weights)
-    # print(sum(weights))
-    num_dat = random.random()
-    for i, cs in enumerate(cum_sum):
-        if i==0 and num_dat<cs:
-            return 1
-        if weights[i-1]<num_dat<cs:
-            #If it's greater, we have gone too far:
-            return i
+def InfluenceFunction(train, dev, test, classifier_algo):
+    #Calculating the values of each point in the training set with influence function
 
+
+    return phis
 
 def main():
     set_seed()
     DATASET_SIZE=100
-    NUM_BOOTSTRAP=25
-
     train, dev, test=initialize_dataset(DATASET_SIZE)
 
     print(f"Initialized SST-2 with length of {len(train)}")
     # classifiers=[Bert_Classifier]
-    classifiers=[Bert_Classifier]
+    classifiers=[RNN_Classifier, Bert_Classifier]
+    lrs=[1e-3, 1e-5]
+    # Bert 1e-5 : 61.3 but overfits
+    # 1e-6: 50.9
+    # 1e-4: 50.9
+    # 1e-3 :50.9
+    # 1e-2
+
     # names=['BERT']
     names=['BERT']
-    for name, classifier_algo in zip(names, classifiers):
-        values=[[] for i in range(DATASET_SIZE)]
+    for name, classifier_algo, lr in zip(names, classifiers, lrs):
+        if name=="RNN":
+            continue
         plt.figure()
         print(f"Running LOO with {name} classifier")
+
         set_seed()
         Classifier = classifier_algo(train)
-        results = Classifier.train_test(train, dev, test)
-        dev_baseline = results[1]
-        test_baseline = results[2]
+        results=Classifier.train_test(train, dev, test)
+        dev_baseline=results[1]
+        test_baseline=results[2]
 
-        for t in range(1, NUM_BOOTSTRAP+1, 1):
-            NUM_DATA_IN_BOOTSTRAP=sample_num_data(DATASET_SIZE)
-            train_iter = copy.deepcopy(train)
-            boostrap = train_iter.bootstrap_data(NUM_DATA_IN_BOOTSTRAP)
-            set_seed(random.randint(0, 10000))
-            Classifier = classifier_algo(train_iter)
-            results=Classifier.train_test(train_iter, dev, test)
-            dev_results=results[1]
+        print(f"Results with all data is {results}")
 
+        results=[]
 
-            for i in range(DATASET_SIZE):
-                if i not in boostrap:
-                    values[i].append(dev_results)
-
-        values=[np.mean(vv) for vv in values]
-        results=values
-        sorted_results=np.argsort(results)
+        shapleys=Gradient_Shapley(train, dev, test, classifier_algo, dev_baseline, lr)
+        sorted_results=np.argsort(shapleys)
         # print(sorted_results)
         # print(sorted_results[::-1])
         results_remove_best=[test_baseline]
         results_remove_worst=[test_baseline]
+        values_x = [0]
+        values_x.extend([i for i in range(5, 55, 5)])
         print("Evaluation of LOO, removing best data by bs of 10")
         for i in range(5, 55, 5):
             train_eval=copy.deepcopy(train)
@@ -101,9 +92,11 @@ def main():
             set_seed()
             train_eval.reset_index()
             Classifier = classifier_algo(train_eval)
-            _, dev_res, test_res = Classifier.train_test(train_eval, dev, test)
+            _, _, test_res = Classifier.train_test(train_eval, dev, test)
             results_remove_best.append(test_res)
             print(f"Results of {test_res}")
+        auc_best = auc(values_x, results_remove_best)
+        print(f"Area under curve is {auc_best}")
         print("Evaluation of LOO, removing worst data by bs of 10")
         for i in range(5, 55, 5):
             train_eval = copy.deepcopy(train)
@@ -117,10 +110,13 @@ def main():
             set_seed()
             train_eval.reset_index()
             Classifier = classifier_algo(train_eval)
-            _, dev_res, _ = Classifier.train_test(train_eval, dev, test)
+            _, _, test_res = Classifier.train_test(train_eval, dev, test)
             results_remove_worst.append(test_res)
             print(f"Results of {test_res}")
+        auc_worst = auc(values_x, results_remove_worst)
+        print(f"Area under curve is {auc_worst}")
 
+        print(f"Final metric: {auc_worst - auc_best}")
 
         X=[i for i in range(0,55,5)]
         X.extend([i for i in range(0,55,5)])
@@ -132,8 +128,8 @@ def main():
         print(Y)
         data_plot=pd.DataFrame({'Number of data points removed': X, 'Accuracy': Y, 'Strategy':strats})
         sns.lineplot(x='Number of data points removed', y='Accuracy', hue='Strategy', data=data_plot)
-        plt.title(f'{name}-DataOOB')
-        plt.savefig(f'{name}DataOOB.png')
+        plt.title(f'{name}-Gradient')
+        plt.savefig(f'{name}Gradient.png')
 
 
 if __name__ == '__main__':
