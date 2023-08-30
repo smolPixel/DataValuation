d4158fbeb0a899fc9b997681e579c10889c428e2
frederic <bemeraude@hotmail.com>
2023-08-30 14:20:23 -0400

    auto commit, minor modification

--- TMCShapley.py
+++ TMCShapley.py
@@ -36,7 +36,7 @@ def TMC_Shapley(train, dev, test, classifier_algo, dev_baseline):
     #dev_baseline= V(D) in paper
 
     #For now let's put PT at 2%, aka, when we get at 2% of the value of dev_baseline we are satisfied
-    PT=0.01
+    PT=0.02
     TRUNC_MAX=5
     ITER_NUM=50
 
