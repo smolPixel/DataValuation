6c3fd374433019a8c706b4301e024d445c6b8218
frederic <bemeraude@hotmail.com>
2023-09-01 12:49:48 -0400

    auto commit, minor modification

--- GradientShapley.py
+++ GradientShapley.py
@@ -37,7 +37,7 @@ def Gradient_Shapley(train, dev, test, classifier_algo, dev_baseline, lr):
     #dev_baseline= V(D) in paper
 
     #For now let's put PT at 2%, aka, when we get at 2% of the value of dev_baseline we are satisfied
-    ITER=10
+    ITER=20
     #baseline is 0.5 for binary dataset
     vals=[0.5 for i in range(len(train))]
     phis=[0 for i in range(len(train))]
